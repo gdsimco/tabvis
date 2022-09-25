a90d2b93a9135699c66c59fe8f840da4dd893e8d
Garrett Simco <garrettsimco@gmail.com>
2022-09-25 15:37:29 -0700

    update colors on file open/change

--- tabvis/extension.js
+++ tabvis/extension.js
@@ -13,21 +13,13 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tabvis" is now active!');
+	console.log('Congratulations, your extension "tabvis" is now active 1!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tabvis.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		setcolor.setColor();
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('This is a test message coming from TabVis!');
+	vscode.window.tabGroups.onDidChangeTabs(e => {
+		if (e && e.opened && (e.opened.length > 0 || e.changed.length > 0)) {
+			setcolor.setColor(vscode.window.activeTextEditor.document.fileName);
+		}
 	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
--- tabvis/setcolor.js
+++ tabvis/setcolor.js
@@ -1,20 +1,19 @@
 const vscode = require('vscode');
 const pastelColor = require("pastel-color");
 
-module.exports.setColor = function() {
-    const pathstr = vscode.window.activeTextEditor.document.fileName;
-    const path = pathstr.split('\\').reverse();
+module.exports.setColor = function(pathstr) {
+    const filepath = pathstr.split('\\').reverse();
 
-    if (path.length <= 1) {
+    if (filepath.length <= 1) {
         return; // This file is in top level directory
     }
 
-    const dir = path[1];
+    const dir = filepath[1];
 
     console.log("dir: " + dir);
     const color = pastelColor.getPastelColor(dir);
-
     const settings = vscode.workspace.getConfiguration("workbench");
     const currentColorCustomization = settings.get("colorCustomizations") || {};
     settings.update("colorCustomizations", {...currentColorCustomization, "tab.activeBackground": color.hex})
+
 }
